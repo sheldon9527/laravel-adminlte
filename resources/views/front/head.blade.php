75f034da6e6f2fc790d9ff161849f92f4dd72762
sheldon_yi <xiaodong.yi@defara.com>
2016-08-03 23:43:50 +0800

    [html] head

--- resources/views/front/head.blade.php
+++ resources/views/front/head.blade.php
@@ -4,8 +4,18 @@
 <meta name="keywords" content="易晓东的博客,易晓东" />
 <meta name="description" content="晓东的博客,易晓东" />
 <link href="/front/css/base.css" rel="stylesheet">
+<!-- <link href="/front/css/about.css" rel="stylesheet">
+<link href="/front/css/book.css" rel="stylesheet">
+<link href="/front/css/case.css" rel="stylesheet"> -->
 <link href="/front/css/index.css" rel="stylesheet">
+<!-- <link href="/front/css/learn.css" rel="stylesheet"> -->
+<!-- <link href="/front/css/mood.css" rel="stylesheet"> -->
+<!-- <link href="/front/css/new.css" rel="stylesheet">
+<link href="/front/css/share.css" rel="stylesheet">
+<link href="/front/css/style.css" rel="stylesheet">
+<link href="/front/css/template.css" rel="stylesheet"> -->
 <script src="/front/js/silder.js"></script>
+<script src="/front/js/modernizr.js"></script>
 <!--[if lt IE 9]>
 <script src="js/modernizr.js"></script>
 <![endif]-->
