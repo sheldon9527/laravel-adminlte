c4d5e26a02e02fd266260f3215a888c1c623a1dc
sheldon_yi <xiaodong.yi@defara.com>
2016-08-04 23:14:52 +0800

    [admin] html

--- resources/views/admin/common/sidebar.blade.php
+++ resources/views/admin/common/sidebar.blade.php
@@ -1,7 +1,5 @@
 <aside class="main-sidebar">
-    <!-- Left side column. contains the sidebar -->
     <div class="slimScrollDiv">
-        <!-- sidebar: style can be found in sidebar.less -->
         <div class="sidebar" id="scrollspy">
             <div class="user-panel">
                 <div class="pull-left image">
@@ -9,11 +7,9 @@
                 </div>
                 <div class="pull-left info">
                     <p></p>
-                    <!-- Status -->
                     <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
                 </div>
             </div>
-            <!-- search form (Optional) -->
             <form action="#" method="get" class="sidebar-form">
                 <div class="input-group">
                     <input type="text" name="q" class="form-control" placeholder="Search..."/>
@@ -22,107 +18,42 @@
                     </span>
                 </div>
             </form>
-            <!-- /.search form -->
-            <!-- Sidebar Menu -->
             <ul class="sidebar-menu">
-              <li class="header">MAIN NAVIGATION</li>
-              <li class="active treeview">
+                <li class="header">管理</li>
+                <li class="active treeview"><a href="#"><i class="fa fa-dashboard"></i> <span>首页</span> <i class="fa fa-angle-left pull-right"></i></a>
+                    <ul class="treeview-menu">
+                        <li class="active"><a href="#"><i class="fa fa-circle-o"></i>最近关注</a></li>
+                        <li><a href="#"><i class="fa fa-circle-o"></i>最近留言</a></li>
+                        <li><a href="#"><i class="fa fa-circle-o"></i>博客PV</a></li>
+                    </ul>
+                </li>
+                <li class="treeview">
                 <a href="#">
-                  <i class="fa fa-dashboard"></i> <span>Dashboard</span> <i class="fa fa-angle-left pull-right"></i>
-                </a>
-                <ul class="treeview-menu">
-                  <li class="active"><a href="#"><i class="fa fa-circle-o"></i> Dashboard v1</a></li>
-                  <li><a href="#"><i class="fa fa-circle-o"></i> Dashboard v2</a></li>
-                </ul>
-              </li>
-              <li class="treeview">
-                <a href="#">
-                  <i class="fa fa-files-o"></i>
-                  <span>Layout Options</span>
-                  <span class="label label-primary pull-right">4</span>
-                </a>
-                <ul class="treeview-menu">
-                  <li><a href="#"><i class="fa fa-circle-o"></i> Top Navigation</a></li>
-                  <li><a href="#"><i class="fa fa-circle-o"></i> Boxed</a></li>
-                  <li><a href="#"><i class="fa fa-circle-o"></i> Fixed</a></li>
-                  <li><a href="#"><i class="fa fa-circle-o"></i> Collapsed Sidebar</a></li>
-                </ul>
-              </li>
-              <li>
-                <a href="#">
-                  <i class="fa fa-th"></i> <span>Widgets</span>
-                  <small class="label pull-right bg-green">new</small>
-                </a>
-              </li>
-              <li class="treeview">
-                <a href="#">
-                  <i class="fa fa-pie-chart"></i>
-                  <span>Charts</span>
-                  <i class="fa fa-angle-left pull-right"></i>
-                </a>
-                <ul class="treeview-menu">
-                  <li><a href="#"><i class="fa fa-circle-o"></i> ChartJS</a></li>
-                  <li><a href="#"><i class="fa fa-circle-o"></i> Morris</a></li>
-                  <li><a href="#"><i class="fa fa-circle-o"></i> Flot</a></li>
-                  <li><a href="#"><i class="fa fa-circle-o"></i> Inline charts</a></li>
-                </ul>
-              </li>
-              <li class="treeview">
-                <a href="#">
-                  <i class="fa fa-laptop"></i>
-                  <span>UI Elements</span>
-                  <i class="fa fa-angle-left pull-right"></i>
-                </a>
-                <ul class="treeview-menu">
-                  <li><a href="#"><i class="fa fa-circle-o"></i> General</a></li>
-                  <li><a href="#"><i class="fa fa-circle-o"></i> Icons</a></li>
-                  <li><a href="#"><i class="fa fa-circle-o"></i> Buttons</a></li>
-                  <li><a href="#"><i class="fa fa-circle-o"></i> Sliders</a></li>
-                  <li><a href="#"><i class="fa fa-circle-o"></i> Timeline</a></li>
-                  <li><a href="#"><i class="fa fa-circle-o"></i> Modals</a></li>
-                </ul>
-              </li>
-              <li class="treeview">
-                <a href="#">
-                  <i class="fa fa-edit"></i> <span>Forms</span>
-                  <i class="fa fa-angle-left pull-right"></i>
-                </a>
-                <ul class="treeview-menu">
-                  <li><a href="#"><i class="fa fa-circle-o"></i> General Elements</a></li>
-                  <li><a href="#"><i class="fa fa-circle-o"></i> Advanced Elements</a></li>
-                  <li><a href="#"><i class="fa fa-circle-o"></i> Editors</a></li>
-                </ul>
-              </li>
-              <li class="treeview">
-                <a href="#">
-                  <i class="fa fa-table"></i> <span>Tables</span>
-                  <i class="fa fa-angle-left pull-right"></i>
-                </a>
-                <ul class="treeview-menu">
-                  <li><a href="#"><i class="fa fa-circle-o"></i> Simple tables</a></li>
-                  <li><a href="#"><i class="fa fa-circle-o"></i> Data tables</a></li>
-                </ul>
-              </li>
-              <li>
-                <a href="#">
-                  <i class="fa fa-envelope"></i> <span>Mailbox</span>
-                  <small class="label pull-right bg-yellow">12</small>
-                </a>
-              </li>
-              <li class="treeview">
-                <a href="#">
-                  <i class="fa fa-folder"></i> <span>Examples</span>
-                  <i class="fa fa-angle-left pull-right"></i>
-                </a>
-                <ul class="treeview-menu">
-                  <li><a href="#"><i class="fa fa-circle-o"></i> Invoice</a></li>
-                  <li><a href="#"><i class="fa fa-circle-o"></i> Profile</a></li>
-                  <li><a href="#"><i class="fa fa-circle-o"></i> Login</a></li>
-                  <li><a href="#"><i class="fa fa-circle-o"></i> Register</a></li>
-                  <li><a href="#"><i class="fa fa-circle-o"></i> Lockscreen</a></li>
-                </ul>
-              </li>
-          </ul>
+                    <i class="fa fa-files-o"></i><span>文章管理</span><span class="label label-primary pull-right"></span></a>
+                    <ul class="treeview-menu">
+                        <li><a href="#"><i class="fa fa-circle-o"></i>添加文章</a></li>
+                        <li><a href="#"><i class="fa fa-circle-o"></i>文章列表</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#"><i class="fa fa-th"></i> <span>个人简历</span><small class="label pull-right bg-green">new</small></a>
+                </li>
+                <li class="treeview">
+                    <a href="#"><i class="fa fa-pie-chart"></i><span>慢生活</span><i class="fa fa-angle-left pull-right"></i></a>
+                    <ul class="treeview-menu">
+                        <li><a href="#"><i class="fa fa-circle-o"></i>关于我</a></li>
+                        <li><a href="#"><i class="fa fa-circle-o"></i>相册</a></li>
+                        <li><a href="#"><i class="fa fa-circle-o"></i>碎言碎语</a></li>
+                    </ul>
+                </li>
+                <li class="treeview">
+                    <a href="#"><i class="fa fa-laptop"></i><span>设置</span><i class="fa fa-angle-left pull-right"></i></a>
+                    <ul class="treeview-menu">
+                        <li><a href="#"><i class="fa fa-circle-o"></i>基本信息</a></li>
+                        <li><a href="#"><i class="fa fa-circle-o"></i>社交账号</a></li>
+                    </ul>
+                </li>
+            </ul>
         </div>
     </div>
 </aside>
