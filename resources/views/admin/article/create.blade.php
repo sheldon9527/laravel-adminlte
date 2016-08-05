40dba01a5103117b5b1bdc4d277e162af9d659b6
sheldon_yi <xiaodong.yi@defara.com>
2016-08-05 18:27:20 +0800

    [admin]

--- resources/views/admin/article/create.blade.php
+++ resources/views/admin/article/create.blade.php
@@ -49,8 +49,8 @@
             </div>
         </div>
     </div>
-<script type="text/javascript" src="/qeditor/ueditor.config.js"></script>
-<script type="text/javascript" src="/qeditor/ueditor.all.js"></script>
+<script type="text/javascript" src="/editor/ueditor.config.js"></script>
+<script type="text/javascript" src="/editor/ueditor.all.js"></script>
 <script type="text/javascript">
 	var ue = UE.getEditor('container');
 </script>
