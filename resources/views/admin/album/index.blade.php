1e8a7bba287f24127bcdc0b29ae755fc40600b28
sheldon9527 <sheldon9527yxd@gmail.com>
2017-01-11 19:18:31 +0800

    [index]

--- resources/views/admin/album/index.blade.php
+++ resources/views/admin/album/index.blade.php
@@ -23,12 +23,13 @@
                                 @else
                                     <img src="/images/album.png" alt="/images/album.png">
                                 @endif
+                                <!-- {{$album->description}} -->
                                   <div class="caption">
                                     <a href="{{route('admin.albums.delete',$album->id)}}" data-method='DELETE' data-confirm="Are you sure?"  class="btn btn-primary"><i class="fa fa-times"></i></a>
                                     <a href="#" data-toggle="modal" data-target="#editAlbumModal"
                                     data-name="{{$album->name}}" data-description="{{$album->description}}"  data-status="{{$album->status}}"  data-front="{{$album->is_front}}" data-href="{{route('admin.albums.update',$album->id)}}"
                                       class="btn btn-primary edit-album"><i class="fa fa-pencil-square-o"></i></a>
-                                    <li data-toggle="modal" data-target="#addPictureModal"  class="btn btn-primary"><i class="fa fa-plus"></i></li>
+                                    <li data-toggle="modal" data-target="#addPictureModal"  class="btn bg-green pull-right"><i class="fa fa-plus"></i></li>
                                   </div>
                             </div>
                           </div>
@@ -60,19 +61,19 @@
                         <div class="form-group">
                             <label class="col-sm-2 control-label"><span class="text-red">*</span>状态</label>
                             <div class="col-sm-8">
-                                <select name="status" class="selectpicker" data-width="auto">
-                                    <option value="active">显示</option>
-                                    <option value="inactive">不显示</option>
-                                </select>
+                                <input type="radio" name="status" value="active">
+                                <label for="" class="padding_right" >激活</label>
+                                <input type="radio" name="status" value="inactive" checked="checked">
+                                <label for="" class="padding_right" >禁用</label>
                             </div>
                         </div>
                         <div class="form-group">
-                            <label class="col-sm-2 control-label"><span class="text-red">*</span>置顶</label>
+                            <label class="col-sm-2 control-label"><span class="text-red">*</span>是否置顶</label>
                             <div class="col-sm-8">
-                                <select name="is_front" class="selectpicker" data-width="auto">
-                                    <option value=1>是</option>
-                                    <option value=0>否</option>
-                                </select>
+                                <input type="radio" name="is_front" value=1 >
+                                <label for="" class="padding_right" >是</label>
+                                <input type="radio" name="is_front" value=0 checked="checked">
+                                <label for="" class="padding_right" >否</label>
                             </div>
                         </div>
                     </div>
@@ -109,16 +110,16 @@
                                 <input type="radio" name="status" value="active" data-name='active'>
                                 <label for="" class="padding_right" >激活</label>
                                 <input type="radio" name="status" value="inactive" data-name='inactive'>
-                                <label for="" class="padding_right" >未激活</label>
+                                <label for="" class="padding_right" >禁用</label>
                             </div>
                         </div>
                         <div class="form-group">
-                            <label class="col-sm-2 control-label"><span class="text-red">*</span>置顶</label>
+                            <label class="col-sm-2 control-label"><span class="text-red">*</span>是否置顶</label>
                             <div class="col-sm-8">
                                 <input type="radio" name="is_front" value=1 data-name=1>
-                                <label for="" class="padding_right" >置顶</label>
+                                <label for="" class="padding_right" >是</label>
                                 <input type="radio" name="is_front" value=0 data-name=0>
-                                <label for="" class="padding_right" >不置顶</label>
+                                <label for="" class="padding_right" >否</label>
                             </div>
                         </div>
                     </div>
@@ -131,10 +132,32 @@
         </form>
     </div>
 
+<!-- addPictureModal -->
+    <div class="modal fade" id="addPictureModal" tabindex="-1" role="dialog" aria-labelledby="addPictureModal">
+        <form action="" method="POST" class="form-horizontal" enctype="multipart/form-data" >
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-body">
+                        <div class="form-group">
+                            <label class="col-sm-2 control-label"><span class="text-red">*</span>名称:</label>
+                            <div class="col-sm-8">
+                                @include('admin.album.pictures.attachment', ['tag' => 'draftFirst', 'attachments' => '', 'modal_id' => 'defaraFirstModal'])
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">取消</button>
+                        <button type="submit" class="btn btn-primary">保存</button>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
+
     <script type="text/javascript">
         require(['jquery'], function($) {
-
-            // 修改子分类
+            // 修改
             $('.caption').on('click', '.edit-album', function () {
                 var name = $(this).data('name');
                 var description = $(this).data('description');
@@ -142,9 +165,6 @@
                 var is_front = $(this).data('front');
                 var href = $(this).data('href');
                 var $model = $('#editAlbumModal');
-                console.log(description);
-                console.log(status);
-                console.log(is_front);
 
                 $model.find('input[name=status]').each(function(){
                   if($(this).data('name') == status){
